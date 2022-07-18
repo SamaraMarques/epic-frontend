84267bc27cc01fe81a5004c0cd3cbf26f9e10d14
Samara Marques <samarabuenomarques@gmail.com>
2022-07-18 03:18:44 -0300

    adjusts result page

--- pages/analyses/[analysis_id]/result.js
+++ pages/analyses/[analysis_id]/result.js
@@ -11,6 +11,7 @@ const AnalysisResult = () => {
   const router = useRouter();
   const { analysis_id } = router.query;
   const [result, setResult] = useState(null);
+  const [enterpriseIndex, setEnterpriseIndex] = useState('');
   const [token, setToken] = useState('');
 
   if (typeof window !== 'undefined') {
@@ -24,12 +25,15 @@ const AnalysisResult = () => {
       .get(`/analyses/${analysis_id}/result`, {
         headers: { Authorization: `Bearer ${token}` },
       })
-      .then((response) => setResult(response.data))
+      .then((response) => {
+        setResult(response.data);
+        setEnterpriseIndex(defineClassificacao(response.data.enterprise.index));
+      })
       .catch((err) => {
         console.log(err);
         router.push(`/enterprises`);
       });
-  }, [token, router, analysis_id]);
+  }, [token, router, analysis_id, setResult, setEnterpriseIndex]);
 
   return (
     <Box>
@@ -38,14 +42,15 @@ const AnalysisResult = () => {
       </Head>
       <Stack m={3} direction="column">
         <Typography variant="h5">Resultado da análise</Typography>
-        <Typography mt={3}>{`Empresa de nome ${result?.enterprise.name.replace(
-          /^\w/,
-          (c) => c.toUpperCase(),
-        )} apresentou índice de segurança ${
-          result?.enterprise.index
-        } implementado, considerado ${defineClassificacao(
-          result?.enterprise.index,
-        )}`}</Typography>
+        {result && (
+          <Typography
+            mt={3}
+          >{`Empresa de nome ${result?.enterprise.name.replace(/^\w/, (c) =>
+            c.toUpperCase(),
+          )} apresentou índice de segurança ${
+            result?.enterprise.index
+          } implementado, considerado ${enterpriseIndex}`}</Typography>
+        )}
         <Typography variant="h6" mt={3} mb={2}>
           {'Resultados dos cálculos de conformidade dos setores:'}
         </Typography>
