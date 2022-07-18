429a7acbabc3cf05de94f60816808a0388fbcf48
Samara Marques <samarabuenomarques@gmail.com>
2022-07-17 22:41:24 -0300

    adjusts axios client

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  withCredentials: true,
+  withCredentials: false,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
